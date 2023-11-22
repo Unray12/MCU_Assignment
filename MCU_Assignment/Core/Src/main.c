67cbec0c0208b01ccf4f9f961798cf026029f2b8
Unray12 <thanhtinh1234789@gmail.com>
2023-11-22 09:04:56 +0700

    update

--- MCU_Assignment/Core/Src/main.c
+++ MCU_Assignment/Core/Src/main.c
@@ -207,7 +207,7 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pins : A0_Pin A1_Pin A2_Pin */
   GPIO_InitStruct.Pin = A0_Pin|A1_Pin|A2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pins : D12_Pin D7_Pin D2_Pin */
@@ -220,7 +220,7 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin : A3_Pin */
   GPIO_InitStruct.Pin = A3_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(A3_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pins : D6_Pin D3_Pin D5_Pin D4_Pin */
